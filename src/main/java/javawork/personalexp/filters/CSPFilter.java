4a102352e722d2b3e6fa5a946e659938278775cb
gamal2421 <gamal29211@gmail.com>
2025-06-11 02:08:36 +0300

    ADDED NEW DOM

--- src/main/java/javawork/personalexp/filters/CSPFilter.java
+++ src/main/java/javawork/personalexp/filters/CSPFilter.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 public class CSPFilter implements Filter {
 	
-	public static final String POLICY = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://fpyf8.com https://grookilteepsou.net https://glempirteechacm.com https://roagrofoogrobo.com https://eehassoosostoa.com https://tzegilo.com 'unsafe-inline'; connect-src 'self' https://vaimucuvikuwu.net https://my.rtmark.net https://grookilteepsou.net https://roagrofoogrobo.com https://glempirteechacm.com https://fleraprt.com; object-src 'none'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://fonts.gstatic.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; img-src 'self' data: https://e7.pngegg.com; frame-ancestors 'self'";
+	public static final String POLICY = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://fpyf8.com https://grookilteepsou.net https://glempirteechacm.com https://roagrofoogrobo.com https://eehassoosostoa.com https://tzegilo.com https://fenoofaussut.net 'unsafe-inline'; connect-src 'self' https://vaimucuvikuwu.net https://my.rtmark.net https://grookilteepsou.net https://roagrofoogrobo.com https://glempirteechacm.com https://fleraprt.com https://bobapsoabauns.com; object-src 'none'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://fonts.gstatic.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; img-src 'self' data: https://e7.pngegg.com https://bobapsoabauns.com; frame-ancestors 'self'";
 
 	@Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
