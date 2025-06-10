71ac98184d7470a37815adbbfaeaf7bf35817b5b
gamal2421 <gamal29211@gmail.com>
2025-06-11 02:12:28 +0300

    Changes AT CSP filter to allow 'self' and 'unsafe-inline' for script-src

--- src/main/java/javawork/personalexp/filters/CSPFilter.java
+++ src/main/java/javawork/personalexp/filters/CSPFilter.java
@@ -11,8 +11,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CSPFilter implements Filter {
+//	public static final String POLICY = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://fpyf8.com https://grookilteepsou.net https://glempirteechacm.com https://roagrofoogrobo.com https://eehassoosostoa.com https://tzegilo.com https://fenoofaussut.net 'unsafe-inline'; connect-src 'self' https://vaimucuvikuwu.net https://my.rtmark.net https://grookilteepsou.net https://roagrofoogrobo.com https://glempirteechacm.com https://fleraprt.com https://bobapsoabauns.com; object-src 'none'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://fonts.gstatic.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; img-src 'self' data: https://e7.pngegg.com https://bobapsoabauns.com; frame-ancestors 'self'";
 	
-	public static final String POLICY = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://fpyf8.com https://grookilteepsou.net https://glempirteechacm.com https://roagrofoogrobo.com https://eehassoosostoa.com https://tzegilo.com https://fenoofaussut.net 'unsafe-inline'; connect-src 'self' https://vaimucuvikuwu.net https://my.rtmark.net https://grookilteepsou.net https://roagrofoogrobo.com https://glempirteechacm.com https://fleraprt.com https://bobapsoabauns.com; object-src 'none'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://fonts.gstatic.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; img-src 'self' data: https://e7.pngegg.com https://bobapsoabauns.com; frame-ancestors 'self'";
+	public static final String POLICY = "default-src *; script-src * 'unsafe-inline'; connect-src *; object-src *; style-src * 'unsafe-inline'; font-src *; img-src * data:; frame-ancestors *";
 
 	@Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
