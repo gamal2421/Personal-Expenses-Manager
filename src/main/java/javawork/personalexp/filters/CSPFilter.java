f7e5689cacfc0b79c2201c9e0fda7c65e951cb88
gamal2421 <gamal29211@gmail.com>
2025-06-11 01:55:18 +0300

    CSPFilter Changes

--- src/main/java/javawork/personalexp/filters/CSPFilter.java
+++ src/main/java/javawork/personalexp/filters/CSPFilter.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 public class CSPFilter implements Filter {
 	
-	public static final String POLICY = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net; object-src 'none'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://fonts.gstatic.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; img-src 'self' data: https://e7.pngegg.com; frame-ancestors 'self'";
+	public static final String POLICY = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://fpyf8.com; object-src 'none'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://fonts.gstatic.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; img-src 'self' data: https://e7.pngegg.com; frame-ancestors 'self'";
 
 	@Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
